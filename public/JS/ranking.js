62b89d84ea8c6c9f4a1d4ac90ebb2d9fe3f68be3
Marioboto3 <marioboto98@gmail.com>
2019-06-16 20:48:54 +0200

    pruebas ranking

--- public/JS/ranking.js
+++ public/JS/ranking.js
@@ -3,13 +3,13 @@ $(document).ready(function() {
 
     $.get("http://147.83.7.205:8080/dsaApp/user/ranking", function (data) {
         var i=1;
-        console.log("Data:"+data);
+        console.log("Data:"+data.rankings);
         while (i < 10) {
-            var name = data.Object.rankings[i].name;
-            var currentTime = data.Object.rankings[i].currentTime;
-            var currentEnemiesKilled = data.Object.rankings[i].currentEnemiesKilled;
-            var currentLife = data.Object.rankings[i].currentLife;
-            console.log("Ranking:", data);
+            var name = data.rankings.name;
+            var currentTime = data.rankings.currentTime;
+            var currentEnemiesKilled = data.rankings.currentEnemiesKilled;
+            var currentLife = data.rankings.currentLife;
+            console.log("Ranking:", data.rankings);
             var insertion = "<tr><td>" + name + "</td>><td>" + currentTime + "</td></tr><tr><td>" + currentEnemiesKilled + "</td><td>" + currentLife + "</td></tr>";
             $("#mytablaranking body").append(insertion);
             i++;
