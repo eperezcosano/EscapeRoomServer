3d5d8bbcefb5900ca078fd7604bf62644664ec6d
Marioboto3 <marioboto98@gmail.com>
2019-06-16 21:22:49 +0200

    prubas ranking

--- public/JS/ranking.js
+++ public/JS/ranking.js
@@ -6,13 +6,13 @@ $(document).ready(function() {
         console.log("Data:"+data);
         while (i < 10) {
             var name = data[i].name;
-            console.log("1:"+data[i].name);
+            console.log("1:"+name);
             var currentTime = data[i].currentTime;
-            console.log("2:"+data[i].currentTime);
+            console.log("2:"+currentTime);
             var currentEnemiesKilled = data[i].currentEnemiesKilled;
-            console.log("3:"+data[i].currentEnemiesKilled);
+            console.log("3:"+currentEnemiesKilled);
             var currentLife = data[i].currentLife;
-            console.log("Ranking:", data[i].currentLife);
+            console.log("Ranking:", currentLife);
             var insertion = "<tr><td>" + name + "</td><td>" + currentTime + "</td><td>" + currentEnemiesKilled + "</td><td>" + currentLife + "</td></tr>";
             $("#mytablarankingid body").append(insertion);
             i++;
