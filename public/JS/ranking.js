b22b8832c406e0e190c7194aab40f92751c35f97
Marioboto3 <marioboto98@gmail.com>
2019-06-16 18:36:15 +0200

    Ranking web

--- public/JS/ranking.js
+++ public/JS/ranking.js
@@ -1,13 +1,15 @@
 
 $(document).ready(function() {
 
-    while(i<data.lista.length) {
-        var name = data.name;
-        var currentTime = data.currentTime;
-        var currentEnemiesKilled = data.currentEnemiesKilled;
-        var currentLife = data.currentLife;
-        console.log("Ranking:", data);
-     var insertion = "<tr><td>" + name + "</td>><td>" + currentTime + "</td></tr><tr><td>" + currentEnemiesKilled + "</td><td>" + currentLife + "</td></tr>";
-    $("#mytablaranking body").append(insertion);
-}
+    $.get("http://147.83.7.205:8080/dsaApp/user/ranking", function (data) {
+        while (i < data.lista.length) {
+            var name = data.name;
+            var currentTime = data.currentTime;
+            var currentEnemiesKilled = data.currentEnemiesKilled;
+            var currentLife = data.currentLife;
+            console.log("Ranking:", data);
+            var insertion = "<tr><td>" + name + "</td>><td>" + currentTime + "</td></tr><tr><td>" + currentEnemiesKilled + "</td><td>" + currentLife + "</td></tr>";
+            $("#mytablaranking body").append(insertion);
+        }
+    })
 })
