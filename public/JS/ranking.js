5139ce28ffe466a56222b989defaa344ec39b720
Marioboto3 <marioboto98@gmail.com>
2019-06-16 21:26:01 +0200

    pruebas ranking

--- public/JS/ranking.js
+++ public/JS/ranking.js
@@ -2,20 +2,20 @@
 $(document).ready(function() {
 
     $.get("http://147.83.7.205:8080/dsaApp/user/ranking", function (data) {
-        var i=1;
-        console.log("Data:"+data);
+        var i = 1;
+        console.log("Data:" + data);
         while (i < 10) {
             var name = data[i].name;
-            console.log("1:"+name);
+            console.log("1:" + name);
             var currentTime = data[i].currentTime;
-            console.log("2:"+currentTime);
+            console.log("2:" + currentTime);
             var currentEnemiesKilled = data[i].currentEnemiesKilled;
-            console.log("3:"+currentEnemiesKilled);
+            console.log("3:" + currentEnemiesKilled);
             var currentLife = data[i].currentLife;
             console.log("Ranking:", currentLife);
             var insertion = "<tr><td>" + name + "</td><td>" + currentTime + "</td><td>" + currentEnemiesKilled + "</td><td>" + currentLife + "</td></tr>";
             $("#mytablarankingid body").append(insertion);
             i++;
         }
-    })
-})
+    }, "json");
+});
