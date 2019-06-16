db16acc166489467a8100c2be9cbcecd272a0b05
Marioboto3 <marioboto98@gmail.com>
2019-06-16 21:06:32 +0200

    gol

--- public/JS/ranking.js
+++ public/JS/ranking.js
@@ -5,13 +5,13 @@ $(document).ready(function() {
         var i=1;
         console.log("Data:"+data);
         while (i < 10) {
-            var name = data[i].ranking;
-            console.log("1:"+data[i].ranking);
+            var name = data[i].name;
+            console.log("1:"+data[i].name);
             var currentTime = data[i].currentTime;
             console.log("2:"+data[i].currentTime);
-            var currentEnemiesKilled = data[i].rankings.currentEnemiesKilled;
-            console.log("3:"+data[i].rankings.currentEnemiesKilled);
-            var currentLife = data.rankings.currentLife;
+            var currentEnemiesKilled = data[i].currentEnemiesKilled;
+            console.log("3:"+data[i].currentEnemiesKilled);
+            var currentLife = data[i].currentLife;
             console.log("Ranking:", data.rankings);
             var insertion = "<tr><td>" + name + "</td>><td>" + currentTime + "</td></tr><tr><td>" + currentEnemiesKilled + "</td><td>" + currentLife + "</td></tr>";
             $("#mytablaranking body").append(insertion);
