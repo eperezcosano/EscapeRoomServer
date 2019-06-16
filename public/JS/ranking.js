f4de78e3c01d158b937faf1a407cfe23942990e3
Marioboto3 <marioboto98@gmail.com>
2019-06-16 18:46:30 +0200

    Prueba ran

--- public/JS/ranking.js
+++ public/JS/ranking.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
     $.get("http://147.83.7.205:8080/dsaApp/user/ranking", function (data) {
         var i=0;
         console.log("Data:"+data);
-        while (i < data.length) {
-            var name = data.name;
-            var currentTime = data.currentTime;
-            var currentEnemiesKilled = data.currentEnemiesKilled;
-            var currentLife = data.currentLife;
+        while (i < 10) {
+            var name = data.ranking[i].name;
+            var currentTime = data.ranking[i].currentTime;
+            var currentEnemiesKilled = data.ranking[i].currentEnemiesKilled;
+            var currentLife = data.ranking[i].currentLife;
             console.log("Ranking:", data);
             var insertion = "<tr><td>" + name + "</td>><td>" + currentTime + "</td></tr><tr><td>" + currentEnemiesKilled + "</td><td>" + currentLife + "</td></tr>";
             $("#mytablaranking body").append(insertion);
