b339099f4693f9283f7797a1fafb36b69699f5ee
Marioboto3 <marioboto98@gmail.com>
2019-06-17 12:02:59 +0200

    Ranking

--- public/JS/ranking.js
+++ public/JS/ranking.js
@@ -14,7 +14,8 @@ $(document).ready(function() {
             var currentLife = data[i].currentLife;
             console.log("Ranking:", currentLife);
             var insertion = "<tr><td>" + name + "</td><td>" + currentTime + "</td><td>" + currentEnemiesKilled + "</td><td>" + currentLife + "</td></tr>";
-            $("#mytablarankingid body").append(insertion);
+           console.log("Insertion:"+insertion);
+            $("#rankingid tbody").append(insertion);
             i++;
         }
     }, "json");
